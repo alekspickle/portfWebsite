4975319b916c278ddf0e2bed6169a2d9e3f4a5e8
Alex gnatrow <22867443+alexgnatrow@users.noreply.github.com>
2017-11-22 15:24:45 +0200

    Github Upload
    Uploaded row skeleton

---
+++ port.js
@@ -0,0 +1,10 @@
+var buttons = document.querySelectorAll('.button');
+
+for (var i = 0; i < buttons.length;i++){
+
+buttons[i].addEventListener("click",function(){
+  this.disabled = true;
+
+});
+
+}
